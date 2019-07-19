4a313b4e128095b7d3c3b4af7eb207b92a7132ff
Mrhrh <51903707+MrHrh@users.noreply.github.com>
2019-07-19 23:00:19 +0800

    Create code13.cpp

---
+++ code13.cpp
@@ -0,0 +1,36 @@
+/*
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""。
+示例 1:
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+来源：力扣（LeetCode）
+*/
+
+//-------------------------------------------------------------------------
+
+//code
+
+class Solution {
+public:
+   string longestCommonPrefix(vector<string>& strs) {
+       if(strs.size() == 0) return "";
+       string s;
+       int i = 0;
+       for (; i < strs[0].size(); i++)
+       {
+           int j = 1;
+           for (; j < strs.size(); j++)
+           {
+               if (i < strs[j].size() && strs[j][i] == strs[j - 1][i]) continue;
+               else return s;
+           }
+           s += strs[j-1][i];
+       }
+       return s;
+   }
+};
