03207a56a551c4cbeb36d29be74eea9c643fe024
Mrhrh <51903707+MrHrh@users.noreply.github.com>
2019-08-24 11:14:05 +0800

    Create code17.py

---
+++ code17.py
@@ -0,0 +1,22 @@
+"""
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+输入："23"
+输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+"""
+#code
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        d = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        n = len(digits)
+        res = ['']
+        for i in range(n):
+            res = [x + y for x in res for y in d[digits[i]]]
+        return res
